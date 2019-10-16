3b5e6a017f3a6980f743e74d7c7f7ef53c5c1be0
TrungTho <41388666+TrungTho@users.noreply.github.com>
2019-10-16 22:48:39 +0700

    init func and UI
    5 chức năng cơ bản

---
+++ Project01_BatchRename/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project01_BatchRename
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //POCO
+        class FileName
+        {
+            public string name { get; set; }
+        }
+
+        //Source of truth - process later :)))
+        List<FileName> _fileName = null;
+
+        class FileNameDao
+        {
+            //get data from database here (...)
+        }
+
+        class FileNameBus
+        {
+            //modify data here
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
