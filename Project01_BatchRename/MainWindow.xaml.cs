102ff8962ab0be2aac5b210fa7dfa0d9e1b92d69
TrungTho <41388666+TrungTho@users.noreply.github.com>
2019-11-09 17:57:11 +0700

    missing: moveFunc, getItem, UI, preset

--- Project01_BatchRename/MainWindow.xaml.cs
+++ Project01_BatchRename/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -27,20 +30,78 @@ namespace Project01_BatchRename
         //POCO
         class FileName
         {
-            public string name { get; set; }
+            public int id { get; set; }
+            public string oldName { get; set; }
+            public string filePath { get; set; }
+            public string newName { get; set; }
+            public string err { get; set; }
         }
 
-        //Source of truth - process later :)))
-        List<FileName> _fileName = null;
+        BindingList<FileName> _fileNames = new BindingList<FileName>(); //list of file loaded to listview
 
-        class FileNameDao
+        //operate operations to data
+        private void previewOperations()
         {
-            //get data from database here (...)
+            foreach (var datum in _fileNames)
+            {
+                foreach (var oper in readyLoadOper)
+                {
+                    
+                }
+            }
         }
 
-        class FileNameBus
+        List<StringOperations> _prototypes = new List<StringOperations>(); //list of operations can perform
+        BindingList<StringOperations> readyLoadOper = new BindingList<StringOperations>(); //list of operation that was chosen
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            //modify data here
+            //load operations to prototype to show in combobox
+            var prototype1 = new Replace()
+            {
+                Arguments = new ReplaceArguments()
+                {
+                    oldPattern = "",
+                    newPattern = ""
+                }
+            };
+
+            var prototype2 = new NewCase()
+            {
+                Arguments = new NewCaseArguments()
+                {
+                    typeOfNewCase = 1
+                }
+            };
+
+            var prototype3 = new Normalize()
+            {
+                Arguments = new NormalizeArguments() { }
+            };
+
+            var prototype4 = new MoveCharacters()
+            {
+                Arguments = new MoveCharactersArguments()
+                {
+                    numbersOfChar = 13,
+                    typeOfMove = 1
+                }
+            };
+
+            var prototype5 = new GUIDGenerate()
+            {
+                Arguments = new GUIDGenerateArguments() { }
+            };
+
+            _prototypes.Add(prototype1);
+            _prototypes.Add(prototype2);
+            _prototypes.Add(prototype3);
+            _prototypes.Add(prototype4);
+            _prototypes.Add(prototype5);
+
+            comboBoxToChooseOperations.ItemsSource = _prototypes;
+
+            listBoxOperations.ItemsSource = readyLoadOper;
+
         }
 
         public MainWindow()
@@ -48,126 +109,147 @@ namespace Project01_BatchRename
             InitializeComponent();
         }
 
-        private void CreateGUIDNameButton_Click(object sender, RoutedEventArgs e)
+        private void addOperation_Click(object sender, RoutedEventArgs e)
         {
-            desFileName.Text = Guid.NewGuid().ToString();
+            StringOperations newOper = comboBoxToChooseOperations.SelectedItem as StringOperations;
+            if (newOper != null)
+                readyLoadOper.Add(newOper.Clone());
+            else
+                MessageBox.Show("Please choose Operation to add!");
         }
 
-        private void ReplaceFileNameButton_Click(object sender, RoutedEventArgs e)
+        private void downButton(object sender, RoutedEventArgs e)
         {
-            string pattern = txtPattern.Text;
-            string src = srcFileName.Text;
-            string newPattern = txtNewPattern.Text;
-            string tmp = "";
-            RegexOptions options = RegexOptions.Multiline | RegexOptions.IgnoreCase;
-            var myBuilder = new StringBuilder();
-            int startPos = 0;
-
-            foreach (Match m in Regex.Matches(src,pattern,options))
+            if (listBoxOperations.SelectedIndex < readyLoadOper.Count - 1)
             {
-                tmp = src.Substring(startPos, m.Index-startPos);
-                myBuilder.Append(tmp+newPattern);
-                startPos = m.Index + m.Value.Length;
+                var _tmp = listBoxOperations.SelectedItem as StringOperations;
+                int index = listBoxOperations.SelectedIndex;
+                readyLoadOper.RemoveAt(index);
+                readyLoadOper.Insert(index + 1, _tmp);
+                listBoxOperations.SelectedIndex = index + 1;
             }
-            tmp = src.Substring(startPos);
-            myBuilder.Append(tmp);
-
-            desFileName.Text = myBuilder.ToString();
+            else
+                if (readyLoadOper.Count == 0)
+                    MessageBox.Show("Nothing to move!!!");
+                else
+                    MessageBox.Show("Ouch!!! Can't move down anymore!");
         }
 
-        private void CreateNewCaseButton_Click(object sender, RoutedEventArgs e)
+        private void upButton(object sender, RoutedEventArgs e)
         {
-            var screen = new NewCaseOptions();
-            screen.BoxChecked += SetCaseMode;
-            if (screen.ShowDialog() == true)
+            if (listBoxOperations.SelectedIndex > 0)
             {
-                switch (recaseMode)
+                var _tmp = listBoxOperations.SelectedItem as StringOperations;
+                int index = listBoxOperations.SelectedIndex;
+                readyLoadOper.RemoveAt(index);
+                readyLoadOper.Insert(index - 1, _tmp);
+                listBoxOperations.SelectedIndex = index - 1;
+            }
+            else
+                if (readyLoadOper.Count == 0)
+                    MessageBox.Show("Nothing to move!!!");
+                else
+                    MessageBox.Show("Ouch!!! Can't move up anymore!");
+        }
+        
+        //main click to start program
+        //start to apply all change to all files/folders in listview
+        private void fireStartBatchEvent(object sender, RoutedEventArgs e)
+        {
+
+        }
+        //save preset to an simple txt file
+        private void savePresetButton(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //load preset(template of a chain of operations) in a simple txt file
+        private void loadPresetButton(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void clearAllOperationsButton(object sender, RoutedEventArgs e)
+        {
+            while (readyLoadOper.Count != 0)
+                readyLoadOper.RemoveAt(0);
+            MessageBox.Show("Remove All Operations from ListBox");
+        }
+
+        private void resetAllList()
+        {
+            while (_fileNames.Count != 0)//reset listview and list of data
+                _fileNames.RemoveAt(0);
+            while (readyLoadOper.Count != 0) //clear all operations in listbox
+                readyLoadOper.RemoveAt(0);
+        }
+
+        //private string getShortName(string path, string fullname)
+        //{
+        //    return fullname.Substring(fullname.LastIndexOf(@"\") + 1);
+        //}
+
+        private void loadFilesButton(object sender, RoutedEventArgs e)
+        {
+            resetAllList();
+
+            var screen = new CommonOpenFileDialog(); // show dialog to chose directory
+            screen.IsFolderPicker = true; //directory pick option
+
+            if (screen.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                int tmpID = 0;
+
+                string[] _tmpFiles= Directory.GetFiles(screen.FileName); //get all folders in directory
+                foreach (var newFile in _tmpFiles)  //add to data list
                 {
-                    case 1:
-                        desFileName.Text = srcFileName.Text.ToUpper();
-                        break;
-                    case 2:
-                        desFileName.Text = srcFileName.Text.ToLower();
-                        break;
-                    case 3:
-                        string tmp = srcFileName.Text.ToLower();
-                        string firstChar = tmp.Substring(0, 1).ToUpper();
-                        tmp = tmp.Remove(0, 1).Insert(0, firstChar);
-                        desFileName.Text = tmp;
-                        break;
+                    var newItem = new FileName()
+                    {
+                        id = ++tmpID,
+                        filePath = newFile,
+                        oldName = System.IO.Path.GetFileName(newFile),
+                        newName = "",
+                        err = ""
+                    };
+
+                    _fileNames.Add(newItem);
                 }
+
             }
-            else
-            {
-                recaseMode = 0;
-            }
+
+            listviewOfFiles.ItemsSource = _fileNames; //update to UI of listview
         }
 
-        private void SetCaseMode(int caseMode)
+        private void loadFoldersButton(object sender, RoutedEventArgs e)
         {
-            recaseMode = caseMode;
-        }
+            resetAllList(); 
 
-        private void NormalizeFileNameButton_Click(object sender, RoutedEventArgs e)
-        {
-            desFileName.Text = Global.NormalizeString(srcFileName.Text);
-            
-        }
+            var screen = new CommonOpenFileDialog(); // show dialog to chose directory
+            screen.IsFolderPicker = true; //directory pick option
 
-        private void MoveFileNameButton_Click(object sender, RoutedEventArgs e)
-        {
-            bool stat = true;
-            string src = srcFileName.Text;
-            string decDigits = "0123456789";
-            char[] arrDigits = decDigits.ToCharArray();
-            int startPos = src.IndexOfAny(arrDigits);
-            int endPos = 0;
-            if (startPos == -1)
+            if (screen.ShowDialog() == CommonFileDialogResult.Ok)
             {
-                stat = false;
-            }
-            else
-            {
-                int count = 1;
-                endPos = startPos;
-                do
+                int tmpID = 0;
+
+                string[] _tmpFolders = Directory.GetDirectories(screen.FileName); //get all folders in directory
+                foreach (var newFolder in _tmpFolders)  //add to data list
                 {
-                    endPos++;
-
-                    if ((decDigits.IndexOf(src[endPos]) >= 0) || (src[endPos] == '-'))
+                    var newItem = new FileName()
                     {
-                        count++;
-                    }
-                    else
-                    {
-                        break;
-                    }
+                        id = ++tmpID,
+                        filePath = newFolder,
+                        oldName = System.IO.Path.GetFileName(newFolder),
+                        newName = "",
+                        err = ""
+                    };
 
-                } while (true);
-                if (count != 13) stat = false;
-            }
-            if (!stat)
-            {
-                MessageBox.Show("Cannot find ISBN inside file's name!", "Not Found!");
-                return;
+                    _fileNames.Add(newItem);
+                }
+
             }
 
-            string extension = src.Substring(src.LastIndexOf('.'));
-            src = src.Remove(src.LastIndexOf('.'));
-            string ISBN = src.Substring(startPos, endPos - startPos);
-            string bookName;
-
-            if (startPos == 0)
-            {
-                src = src.Remove(endPos, 1);
-                bookName = src.Substring(endPos);
-                desFileName.Text = bookName + " " + ISBN + extension;
-            }
-            else
-            {
-                bookName = src.Substring(0, startPos - 1);
-                desFileName.Text = ISBN + " " + bookName + extension;
-            }
+            listviewOfFiles.ItemsSource = _fileNames; //update to UI of listview
         }
     }
 }
---
+++ Project01_BatchRename/StringOperations.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Project01_BatchRename
+{
+    public class StringArguments
+    {
+        public string Origin { get; set; } //source of data to operate
+    }
+
+   
+    public class ReplaceArguments : StringArguments
+    {
+        public string oldPattern { get; set; } //string to to replaced
+        public string newPattern { get; set; } //string to replace oldPattern
+    }
+
+    public class NewCaseArguments : StringArguments
+    {
+        public int typeOfNewCase { get; set; } //1 - all, low 2 - all up, 3 - normal
+    }
+
+    public class NormalizeArguments : StringArguments
+    {
+
+    }
+       
+    public class MoveCharactersArguments : StringArguments
+    {
+        public int numbersOfChar { get; set; } //numbers of chars to move
+        public int typeOfMove { get; set; } //1 - move to last, 2 - move to first
+    }
+
+    public class GUIDGenerateArguments : StringArguments
+    {
+
+    }
+
+    public abstract class StringOperations
+    {
+        public StringArguments Arguments { get; set; }
+        public abstract string NameOfOperation { get; }
+        public abstract string DescriptionOfOperation { get; }
+
+        public abstract string Operate();
+        public abstract StringOperations Clone();
+    }
+
+    public class Replace : StringOperations
+    {
+        public override string NameOfOperation => "Replace";
+        public override string DescriptionOfOperation
+        {
+            get
+            {
+                var args = Arguments as ReplaceArguments;
+                return $"Replace pattern: {args.oldPattern} with pattern: {args.newPattern}";
+            }
+        }
+
+        public override StringOperations Clone()
+        {
+            var oldArgs = Arguments as ReplaceArguments;
+            return new Replace()
+            {
+                Arguments = new ReplaceArguments()
+                {
+                    Origin = oldArgs.Origin,
+                    oldPattern = oldArgs.oldPattern,
+                    newPattern = oldArgs.newPattern
+                }
+            };
+        }
+
+        public override string Operate()
+        {
+            var args = Arguments as ReplaceArguments;
+            string pattern = args.oldPattern;
+            string src = args.Origin;
+            string newPattern = args.newPattern;
+            string tmp = "";
+            RegexOptions options = RegexOptions.Multiline | RegexOptions.IgnoreCase;
+            var myBuilder = new StringBuilder();
+            int startPos = 0;
+
+            foreach (Match m in Regex.Matches(src, pattern, options))
+            {
+                tmp = src.Substring(startPos, m.Index - startPos);
+                myBuilder.Append(tmp + newPattern);
+                startPos = m.Index + m.Value.Length;
+            }
+            tmp = src.Substring(startPos);
+            myBuilder.Append(tmp);
+
+            return myBuilder.ToString();
+        }
+    }
+
+    public class NewCase : StringOperations
+    {
+        public override string NameOfOperation => "NewCase";
+
+        public override string DescriptionOfOperation
+        {
+            get
+            {
+                var args = Arguments as NewCaseArguments;
+                if (args.typeOfNewCase == 1)
+                    return $"New Case: AllCap";
+                else
+                    if (args.typeOfNewCase == 2)
+                        return $"New Case: AllLow";
+                    else
+                        return $"New Case: Normal";
+            }
+        }
+
+        public override StringOperations Clone()
+        {
+            var oldArgs = Arguments as NewCaseArguments;
+            return new NewCase()
+            {
+                Arguments = new NewCaseArguments()
+                {
+                    Origin = oldArgs.Origin,
+                    typeOfNewCase = oldArgs.typeOfNewCase
+                }
+            };
+        }
+
+        public override string Operate()
+        {
+            var args = Arguments as NewCaseArguments;
+            string _tmp = args.Origin;
+            switch (args.typeOfNewCase)
+            {
+                case 1:
+                    _tmp = _tmp.ToUpper();
+                    break;
+                case 2:
+                    _tmp = _tmp.ToLower();
+                    break;
+                case 3:
+                    _tmp = _tmp.ToLower();
+                    string firstChar = _tmp.Substring(0, 1).ToUpper();
+                    _tmp = _tmp.Remove(0, 1).Insert(0, firstChar); 
+                    break;
+            }
+            return _tmp;
+        }
+    }
+
+    public class Normalize : StringOperations
+    {
+        public override string NameOfOperation => "Normalize";
+
+        public override string DescriptionOfOperation
+        {
+            get
+            {
+                return "Full String Normalize";
+            }
+        }
+
+        public override StringOperations Clone()
+        {
+            var oldArgs = Arguments as NormalizeArguments;
+            return new Normalize()
+            {
+                Arguments = new NormalizeArguments()
+                {
+                    Origin = oldArgs.Origin,
+                }
+            };
+        }
+
+        public override string Operate()
+        {
+            var argr = Arguments as NormalizeArguments;
+            return Global.NormalizeString(argr.Origin);
+        }
+    }
+    public class MoveCharacters : StringOperations
+    {
+        public override string NameOfOperation => "Move ISBN";
+
+        public override string DescriptionOfOperation
+        {
+            get
+            {
+                var args = Arguments as MoveCharactersArguments;
+                if (args.typeOfMove == 1)
+                    return $"Move {args.numbersOfChar} chars to end";
+                else
+                    return $"Move {args.numbersOfChar} chars to begin";
+            }
+        }
+
+        public override StringOperations Clone()
+        {
+            var oldArgs = Arguments as MoveCharactersArguments;
+            return new MoveCharacters()
+            {
+                Arguments = new MoveCharactersArguments()
+                {
+                    Origin = oldArgs.Origin,
+                    typeOfMove=oldArgs.typeOfMove,
+                    numbersOfChar=oldArgs.numbersOfChar
+                }
+            };
+        }
+
+        public override string Operate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class GUIDGenerate : StringOperations
+    {
+        public override string NameOfOperation => "GUID Generate";
+
+        public override string DescriptionOfOperation
+        {
+            get
+            {
+                return "Generate new GUID Name";
+            }
+        }
+
+        public override StringOperations Clone()
+        {
+            var oldArgs = Arguments as GUIDGenerateArguments;
+            return new GUIDGenerate()
+            {
+                Arguments = new GUIDGenerateArguments()
+                {
+                    Origin = oldArgs.Origin
+                }
+            };
+        }
+
+        public override string Operate()
+        {
+            var args = Arguments as GUIDGenerateArguments;
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
