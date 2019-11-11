bff732efd0c481521a574fdf10f3a0cc42dbef56
TrungTho <41388666+TrungTho@users.noreply.github.com>
2019-11-11 23:15:03 +0700

    những thứ tưởng chừng đã mất :)

--- Project01_BatchRename/MainWindow.xaml.cs
+++ Project01_BatchRename/MainWindow.xaml.cs
@@ -25,22 +25,96 @@ namespace Project01_BatchRename
         public int recaseMode = 0; // 0 - Not set, 1 - UPPER, 2 - lower, 3 - Sentence
 
         //POCO
-        class FileName
+        class FileName : INotifyPropertyChanged
         {
+<<<<<<< Updated upstream
             public string name { get; set; }
+=======
+            public int id { get; set; }
+            public string oldName { get; set; }
+            public string filePath { get; set; }
+            public string newName { get; set; }
+            public string err { get; set; }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+            public void updateListviewUI()
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("newName"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("err"));
+            }
+>>>>>>> Stashed changes
         }
 
         //Source of truth - process later :)))
         List<FileName> _fileName = null;
 
+<<<<<<< Updated upstream
         class FileNameDao
         {
             //get data from database here (...)
+=======
+        class FileNameBUS
+        {
+
+>>>>>>> Stashed changes
         }
 
         class FileNameBus
         {
+<<<<<<< Updated upstream
             //modify data here
+=======
+            //load operations to prototype to show in combobox
+            var prototype1 = new Replace()
+            {
+                Arguments = new ReplaceArguments()
+                {
+                    oldPattern = "old",
+                    newPattern = "new"
+                }
+            };
+
+            var prototype2 = new NewCase()
+            {
+                Arguments = new NewCaseArguments()
+                {
+                    isUpper = 1,
+                    isLower = 0,
+                    isSentence=0
+                }
+            };
+
+            var prototype3 = new Normalize()
+            {
+                Arguments = new NormalizeArguments() { }
+            };
+
+            var prototype4 = new MoveCharacters()
+            {
+                Arguments = new MoveCharactersArguments()
+                {
+                    numbersOfChar = 13,
+                    isToFirst = 0,
+                    isToLast=1
+                }
+            };
+
+            var prototype5 = new GUIDGenerate()
+            {
+                Arguments = new GUIDGenerateArguments() { }
+            };
+
+            _prototypes.Add(prototype1);
+            _prototypes.Add(prototype2);
+            _prototypes.Add(prototype3);
+            _prototypes.Add(prototype4);
+            _prototypes.Add(prototype5);
+
+            comboBoxToChooseOperations.ItemsSource = _prototypes;
+            comboBoxToChooseOperations.SelectedIndex= 0;
+
+            listBoxOperations.ItemsSource = readyLoadOper;
+>>>>>>> Stashed changes
         }
 
         public MainWindow()
@@ -51,7 +125,21 @@ namespace Project01_BatchRename
 
         private void CreateGUIDNameButton_Click(object sender, RoutedEventArgs e)
         {
+<<<<<<< Updated upstream
             desFileName.Text = Guid.NewGuid().ToString();
+=======
+            StringOperations newOper = comboBoxToChooseOperations.SelectedItem as StringOperations;
+            if (newOper != null)
+            {
+                readyLoadOper.Add(newOper.Clone());
+                refreshListboxUI();
+                previewOperations();
+            }
+            else
+                MessageBox.Show("Please choose Operation to add!");
+            listBoxOperations.SelectedIndex = listBoxOperations.Items.Count - 1;
+            listBoxOperations.ScrollIntoView(listBoxOperations.SelectedItem);
+>>>>>>> Stashed changes
         }
 
         private void ReplaceFileNameButton_Click(object sender, RoutedEventArgs e)
@@ -99,14 +187,183 @@ namespace Project01_BatchRename
                 }
             }
             else
+<<<<<<< Updated upstream
             {
                 recaseMode = 0;
+=======
+                if (readyLoadOper.Count == 0)
+                    MessageBox.Show("Nothing to move!!!");
+                else
+                    MessageBox.Show("Ouch!!! Can't move up anymore!");
+        }
+
+        private void superdownButton(object sender, RoutedEventArgs e)
+        {
+            if (listBoxOperations.SelectedIndex > 0)
+            {
+                var _tmp = listBoxOperations.SelectedItem as StringOperations;
+                int index = listBoxOperations.SelectedIndex;
+                readyLoadOper.RemoveAt(index);
+                readyLoadOper.Insert(readyLoadOper.Count-1,_tmp);
+                listBoxOperations.SelectedIndex = readyLoadOper.Count-1;
+            }
+            else
+                if (readyLoadOper.Count == 0)
+                MessageBox.Show("Nothing to move!!!");
+            else
+                MessageBox.Show("Ouch!!! Can't move down anymore!");
+        }
+
+        private void superupButton(object sender, RoutedEventArgs e)
+        {
+            if (listBoxOperations.SelectedIndex > 0)
+            {
+                var _tmp = listBoxOperations.SelectedItem as StringOperations;
+                int index = listBoxOperations.SelectedIndex;
+                readyLoadOper.RemoveAt(index);
+                readyLoadOper.Insert(0, _tmp);
+                listBoxOperations.SelectedIndex = 0;
+            }
+            else
+                if (readyLoadOper.Count == 0)
+                    MessageBox.Show("Nothing to move!!!");
+                else
+                    MessageBox.Show("Ouch!!! Can't move up anymore!");
+        }
+
+        private Tuple<string, string> parseName(string filename)
+        {
+            string tmp = filename;
+
+            string shortName= tmp.Substring(0, tmp.LastIndexOf("."));
+            string extensionName = tmp.Substring(tmp.LastIndexOf(".")+1);
+
+            if (tmp == "")
+                return Tuple.Create("", "");
+            else
+                return Tuple.Create( shortName, extensionName);
+        }
+
+        private void checkErr()
+        {
+            for (int i = 0; i < _fileNames.Count; i++)
+            {
+                int countErr = 1;
+                for (int j = i + 1; j < _fileNames.Count; j++)
+                    if ((_fileNames[i].newName == _fileNames[j].newName)&&(_fileNames[i].newName!=""))
+                    {
+                        _fileNames[j].err = $"Duplicate with No. {i}";
+                        var parsedName = parseName(_fileNames[j].newName);
+
+                        _fileNames[j].newName = parsedName.Item1 + countErr.ToString() + "." + parsedName.Item2;
+                        countErr++;
+
+                        _fileNames[j].updateListviewUI();
+                    }
+            }
+
+            
+        }
+
+        //main click to start program
+        //start to apply all change to all files/folders in listview
+        private void fireStartBatchEvent(object sender, RoutedEventArgs e)
+        {
+            foreach (var file in _fileNames)
+            {
+                file.oldName = file.newName;
+            }
+
+        }
+
+        /*combo preset button**/
+        //save preset to an simple txt file
+        private void savePresetButton(object sender, RoutedEventArgs e)
+        {
+            string _tmpInputFilename="preset.txt";
+
+            var screen = new InputFileNameDialog(_tmpInputFilename);
+            if (screen.ShowDialog()==true)
+            {
+                _tmpInputFilename = screen._filename;
+            }
+
+            using (StreamWriter sw = new StreamWriter(_tmpInputFilename))
+            {
+
+                foreach (StringOperations oper in readyLoadOper)
+                {
+                    if (oper.NameOfOperation == "Replace")
+                    {
+                        var _tmpArgs = oper.Arguments as ReplaceArguments;
+                        sw.WriteLine($"Replace {_tmpArgs.oldPattern} {_tmpArgs.newPattern}");
+                    }
+                    else
+                        if (oper.NameOfOperation == "Move ISBN")
+                    {
+                        var _tmpArgs = oper.Arguments as MoveCharactersArguments;
+                        sw.WriteLine($"Move {_tmpArgs.numbersOfChar} {_tmpArgs.isToLast} {_tmpArgs.isToFirst}");
+                    }
+                    else
+                            if (oper.NameOfOperation == "Normalize")
+                    {
+                        var _tmpArgs = oper.Arguments as NormalizeArguments;
+                        sw.WriteLine($"Normalize");
+                    }
+                    else
+                                if (oper.NameOfOperation == "GUID Generate")
+                    {
+                        var _tmpArgs = oper.Arguments as GUIDGenerateArguments;
+                        sw.WriteLine($"GUID");
+                    }
+                    else
+                    {
+                        var _tmpArgs = oper.Arguments as NewCaseArguments;
+                        sw.WriteLine($"NewCase {_tmpArgs.isUpper} {_tmpArgs.isLower} {_tmpArgs.isSentence}");
+
+                    }
+                }
+            }
+
+            MessageBox.Show($"{readyLoadOper.Count} operations was saved!");
+        }
+
+        //load preset(template of a chain of operations) in a simple txt file
+        private void loadPresetButton(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader("textfile.txt"))
+                {
+                    string line;
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+
+                }
+
+                Console.ReadKey();
+            }
+            catch (Exception error)
+            {
+                // thong bao loi.
+                MessageBox.Show("Khong the doc du lieu tu file da cho: ");
+                MessageBox.Show(error.Message);
+>>>>>>> Stashed changes
             }
         }
 
         private void SetCaseMode(int caseMode)
         {
+<<<<<<< Updated upstream
             recaseMode = caseMode;
+=======
+            while (readyLoadOper.Count != 0)
+                readyLoadOper.RemoveAt(0);
+            MessageBox.Show("Removed All Operations from ListBox.", "Alert");
+>>>>>>> Stashed changes
         }
 
         private void NormalizeFileNameButton_Click(object sender, RoutedEventArgs e)
@@ -115,6 +372,7 @@ namespace Project01_BatchRename
             
         }
 
+<<<<<<< Updated upstream
         //private void MoveFileNameButton_Click(object sender, RoutedEventArgs e)
         //{
         //    bool stat = true;
@@ -172,6 +430,9 @@ namespace Project01_BatchRename
         //}
 
         private void MoveFileNameButton_Click(object sender, RoutedEventArgs e)
+=======
+        private void loadFilesButton(object sender, RoutedEventArgs e)
+>>>>>>> Stashed changes
         {
             //"If left blank, move characters at front to last or vice versa. Else, move characters starting from the index to front or to last."
             int numOfChars;
@@ -246,5 +507,110 @@ namespace Project01_BatchRename
             //txtBoxMoveNumChars.Text = srcFileName.Text.Length.ToString();
         }
 
+<<<<<<< Updated upstream
+=======
+        //operate operations to data
+        private void previewOperations()
+        {
+            if (readyLoadOper.Count == 0)
+            {
+                foreach (var file in _fileNames)
+                {
+                    file.newName = file.oldName;
+                    file.updateListviewUI();
+                }
+            }
+            else
+            {
+                bool firstTimeRun = true;
+
+                foreach (var oper in readyLoadOper)
+                {
+                    foreach (var datum in _fileNames)
+                    {
+                        if (firstTimeRun)
+                            oper.Arguments.Origin = datum.oldName;
+                        else
+                            oper.Arguments.Origin = datum.newName;
+
+                        datum.newName = oper.Operate();
+                        datum.updateListviewUI();
+
+
+                        //if (oper.NameOfOperation == "Replace")
+                        //{
+                        //    var _tmpArgs = oper.Arguments as ReplaceArguments;
+                        //    datum.newName = oper.Operate();
+                        //}
+                        //else
+                        //    if (oper.NameOfOperation == "Move ISBN")
+                        //    {
+                        //        var _tmpArgs = oper.Arguments as MoveCharactersArguments;
+                        //        datum.newName = oper.Operate();
+                        //    }
+                        //    else
+                        //        if (oper.NameOfOperation == "Normalize")
+                        //        {
+                        //            var _tmpArgs = oper.Arguments as NormalizeArguments;
+                        //            datum.newName = oper.Operate();
+                        //        }    
+                        //        else
+                        //            if (oper.NameOfOperation == "GUID Generate")
+                        //            {
+                        //                var _tmpArgs = oper.Arguments as GUIDGenerateArguments;
+                        //                datum.newName = oper.Operate();
+                        //            }
+                        //            else
+                        //                {
+                        //                    var _tmpArgs = oper.Arguments as NewCaseArguments;
+                        //                    datum.newName = oper.Operate();
+                        //                }
+                    }
+
+                    firstTimeRun = false;
+
+                    checkErr();
+                }
+            }
+        }
+
+        private void refreshListboxUI()
+        {
+            foreach (var tmp in readyLoadOper)
+                tmp.refreshChanged();
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button buttonClose && buttonClose.Tag is ListBoxItem lsBoxItem)
+            {
+                var operToRemove = lsBoxItem.Content as StringOperations;
+                readyLoadOper.Remove(operToRemove);
+            }
+        }
+
+        private void FileExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            if (MessageBox.Show(this, "Are you sure you want to exit?", "Exit", MessageBoxButton.YesNo) != MessageBoxResult.Yes)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void HelpAbout_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(this, "Nguyễn Khánh Hoàng - 1712457\n      Trần Trung Thọ      - 1712798", "About Us");
+        }
+
+        private void refreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            previewOperations();
+        }
+>>>>>>> Stashed changes
     }
 }
---
+++ Project01_BatchRename/StringOperations.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using PropertyChanged;
+
+namespace Project01_BatchRename
+{
+    public class StringArguments : INotifyPropertyChanged
+    {
+        public string Origin { get; set; } //source of data to operate
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+   
+    public class ReplaceArguments : StringArguments
+    {
+        public string oldPattern { get; set; } //string to to replaced
+        public string newPattern { get; set; } //string to replace oldPattern
+    }
+
+    public class NewCaseArguments : StringArguments
+    {
+        public int isUpper { get; set; } //1 - all, low 2 - all up, 3 - normal
+        public int isLower { get; set; } //1 - all, low 2 - all up, 3 - normal
+        public int isSentence { get; set; } //1 - all, low 2 - all up, 3 - normal
+    }
+
+    public class NormalizeArguments : StringArguments
+    {
+
+    }
+       
+    public class MoveCharactersArguments : StringArguments
+    {
+        public int numbersOfChar { get; set; } //numbers of chars to move
+        public int isToLast { get; set; } //1 - move to last, 2 - move to first
+        public int isToFirst { get; set; } //1 - move to last, 2 - move to first
+    }
+
+    public class GUIDGenerateArguments : StringArguments
+    {
+
+    }
+
+    public abstract class StringOperations
+    {
+        public StringArguments Arguments { get; set; }
+        public abstract string NameOfOperation { get; }
+        public abstract string DescriptionOfOperation { get; }
+
+        public abstract string Operate();
+        public abstract StringOperations Clone();
+        public abstract void refreshChanged();
+    }
+
+    public class Replace : StringOperations, INotifyPropertyChanged
+    {
+        public override string NameOfOperation => "Replace";
+        public override string DescriptionOfOperation
+        {
+            get
+            {
+                var args = Arguments as ReplaceArguments;
+                return $"Replace pattern: {args.oldPattern} with pattern: {args.newPattern}";
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public override StringOperations Clone()
+        {
+            var oldArgs = Arguments as ReplaceArguments;
+            return new Replace()
+            {
+                Arguments = new ReplaceArguments()
+                {
+                    Origin = oldArgs.Origin,
+                    oldPattern = oldArgs.oldPattern,
+                    newPattern = oldArgs.newPattern
+                }
+            };
+        }
+
+        public override string Operate()
+        {
+            var args = Arguments as ReplaceArguments;
+            string pattern = args.oldPattern;
+            string src = args.Origin;
+            string newPattern = args.newPattern;
+            string tmp = "";
+            RegexOptions options = RegexOptions.Multiline | RegexOptions.IgnoreCase;
+            var myBuilder = new StringBuilder();
+            int startPos = 0;
+
+            foreach (Match m in Regex.Matches(src, pattern, options))
+            {
+                tmp = src.Substring(startPos, m.Index - startPos);
+                myBuilder.Append(tmp + newPattern);
+                startPos = m.Index + m.Value.Length;
+            }
+            tmp = src.Substring(startPos);
+            myBuilder.Append(tmp);
+
+            return myBuilder.ToString();
+        }
+
+        public override void refreshChanged()
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("oldPattern"));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("newPattern"));
+        }
+    }
+
+    public class NewCase : StringOperations, INotifyPropertyChanged
+    {
+        public override string NameOfOperation => "NewCase";
+
+        public override string DescriptionOfOperation
+        {
+            get
+            {
+                var args = Arguments as NewCaseArguments;
+                if (args.isUpper == 1)
+                    return $"New Case: AllCap";
+                else
+                    if (args.isLower == 2)
+                    return $"New Case: AllLow";
+                else
+                    return $"New Case: Normal";
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public override StringOperations Clone()
+        {
+            var oldArgs = Arguments as NewCaseArguments;
+            return new NewCase()
+            {
+                Arguments = new NewCaseArguments()
+                {
+                    Origin = oldArgs.Origin,
+                    isLower = oldArgs.isLower,
+                    isSentence = oldArgs.isSentence,
+                    isUpper = oldArgs.isUpper
+                }
+            };
+        }
+
+        public override string Operate()
+        {
+            var args = Arguments as NewCaseArguments;
+            string _tmp = args.Origin;
+            //switch (args.typeOfNewCase)
+            //{
+            //    case 1:
+            //        _tmp = _tmp.ToUpper();
+            //        break;
+            //    case 2:
+            //        _tmp = _tmp.ToLower();
+            //        break;
+            //    case 3:
+            //        _tmp = _tmp.ToLower();
+            //        string firstChar = _tmp.Substring(0, 1).ToUpper();
+            //        _tmp = _tmp.Remove(0, 1).Insert(0, firstChar); 
+            //        break;
+            //}
+            if (args.isUpper == 1)
+                _tmp = _tmp.ToUpper();
+            else
+                if (args.isLower == 1)
+                    _tmp = _tmp.ToLower();
+                else
+                    if (args.isSentence == 1)
+                    {
+                        _tmp = _tmp.ToLower();
+                        string firstChar = _tmp.Substring(0, 1).ToUpper();
+                        _tmp = _tmp.Remove(0, 1).Insert(0, firstChar);
+                    }
+
+            return _tmp;
+        }
+
+        public override void refreshChanged()
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("isUpper"));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("isLower"));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("isSentence"));
+        }
+    }
+
+    public class Normalize : StringOperations, INotifyPropertyChanged
+    {
+        public override string NameOfOperation => "Normalize";
+
+        public override string DescriptionOfOperation
+        {
+            get
+            {
+                return "Full String Normalize";
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public override StringOperations Clone()
+        {
+            var oldArgs = Arguments as NormalizeArguments;
+            return new Normalize()
+            {
+                Arguments = new NormalizeArguments()
+                {
+                    Origin = oldArgs.Origin,
+                }
+            };
+        }
+
+        public override string Operate()
+        {
+            var argr = Arguments as NormalizeArguments;
+            return Global.NormalizeString(argr.Origin);
+        }
+
+        public override void refreshChanged()
+        {
+            return;
+        }
+    }
+    public class MoveCharacters : StringOperations, INotifyPropertyChanged
+    {
+        public override string NameOfOperation => "Move ISBN";
+
+        public override string DescriptionOfOperation
+        {
+            get
+            {
+                var args = Arguments as MoveCharactersArguments;
+                if (args.isToLast == 1)
+                    return $"Move {args.numbersOfChar} chars to end";
+                else
+                    return $"Move {args.numbersOfChar} chars to begin";
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public override StringOperations Clone()
+        {
+            var oldArgs = Arguments as MoveCharactersArguments;
+            return new MoveCharacters()
+            {
+                Arguments = new MoveCharactersArguments()
+                {
+                    Origin = oldArgs.Origin,
+                    isToFirst=oldArgs.isToFirst,
+                    isToLast=oldArgs.isToLast,
+                    numbersOfChar=oldArgs.numbersOfChar
+                }
+            };
+        }
+
+        public override string Operate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void refreshChanged()
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("numbersOfChar"));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("isToLast"));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("isToFirst"));
+        }
+    }
+
+    public class GUIDGenerate : StringOperations, INotifyPropertyChanged
+    {
+        public override string NameOfOperation => "GUID Generate";
+
+        public override string DescriptionOfOperation
+        {
+            get
+            {
+                return "Generate new GUID Name";
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public override StringOperations Clone()
+        {
+            var oldArgs = Arguments as GUIDGenerateArguments;
+            return new GUIDGenerate()
+            {
+                Arguments = new GUIDGenerateArguments()
+                {
+                    Origin = oldArgs.Origin
+                }
+            };
+        }
+
+        public override string Operate()
+        {
+            var args = Arguments as GUIDGenerateArguments;
+            return Guid.NewGuid().ToString();
+        }
+
+        public override void refreshChanged()
+        {
+            return;
+        }
+    }
+}
